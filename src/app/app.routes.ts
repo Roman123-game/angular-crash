53c69653784afc872b143c741a1037c0a32a1144
RB <82284311+Roman123-game@users.noreply.github.com>
2024-01-17 18:46:14 +0200

    update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { TaskComponent } from './components/tasks/tasks.component';
 import { AboutComponent } from './components/about/about.component';
 export const appRoutes: Routes = [
-  { path: '', component: TaskComponent }
+  { path: '', component: TaskComponent },
   { path: 'about', component: AboutComponent },
 ];
 
