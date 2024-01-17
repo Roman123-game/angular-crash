f9da9d9f830bfb3765f040a70fa93ec72023c3b6
RB <82284311+Roman123-game@users.noreply.github.com>
2024-01-17 18:45:20 +0200

    update 5 files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,8 @@ import { HttpClientModule } from '@angular/common/http';
     TaskComponent,
     FontAwesomeModule,
     FormsModule,
-    HttpClientModule ],
+    HttpClientModule,
+ ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { TaskComponent } from './components/tasks/tasks.component';
+import { AboutComponent } from './components/about/about.component';
+export const appRoutes: Routes = [
+  { path: '', component: TaskComponent }
+  { path: 'about', component: AboutComponent },
+];
 
-export const routes: Routes = [];
