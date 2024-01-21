7386d718ca83a2f3d184ecb0d1bcce43edfee37a
RB <82284311+Roman123-game@users.noreply.github.com>
2024-01-21 13:08:43 +0200

    update tasks.component.ts

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -6,6 +6,7 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { TaskServiceService } from '../../services/task-service.service';
 import { AddTodoComponent } from '../add-todo/add-todo.component';
+import { Tasks } from '../../objects/Tasks';
 
 @Component({
   selector: 'app-task',
@@ -22,24 +23,27 @@ export class TaskComponent implements OnInit {
   constructor(private taskService : TaskServiceService){}
 
   ngOnInit(): void {
-   this.taskService.getTasks().subscribe((tasks) =>this.tasks = tasks)
+  //  this.taskService.getTasks().subscribe((tasks) =>this.tasks = tasks)
+  this.tasks = Tasks
   }
 
 
   deleteTask(task: TaskType) {
-    this.taskService
-      .deleteTask(task)
-      .subscribe(
-        () => (this.tasks = this.tasks.filter((t) => t.id !== task.id))
-      );
+    // this.taskService
+    //   .deleteTask(task)
+    //   .subscribe(
+    //     () => (this.tasks = this.tasks.filter((t) => t.id !== task.id))
+    //   );
+      this.tasks = this.tasks.filter((t) => t.id !== task.id)
   }
 
   toggleTask(task:TaskType){
     task.reminder = !task.reminder
-    this.taskService.toggleTaskService(task).subscribe()
+    // this.taskService.toggleTaskService(task).subscribe()
   }
 
   addTask(task:TaskType){
-    this.taskService.addTask(task).subscribe((task) => this.tasks.push(task))
+    // this.taskService.addTask(task).subscribe((task) => this.tasks.push(task))
+    this.tasks.push(taskö)
   }
 }
